4c015624cd3feabe35e1240e7eb9ae291170120d
Erin-Weadick <erin.weadick.2022@mumail.ie>
2024-12-19 11:19:08 +0000

    update

--- R/hello.R
+++ R/hello.R
@@ -12,15 +12,17 @@ data <- read_csv("~/pk01/R/Advanced r programming dataset(in).csv")
 
 utils::globalVariables(c("x", "y", "SPAD", "chlorophyll"))
 
-## Function 1
+## Function 1 : Making column names the same and removing any NA values
 
 tidy_data <- function(data) {
-  if (!is.data.frame(data)) {
-    stop("Input must be a data frame")
-  }
-  tidied_data <- data %>%
+
+    if (!is.data.frame(data)) {
+      stop("Input must be a data frame")
+    }
+
+   tidied_data <- data %>%
     na.omit(data)%>%
-    janitor::clean_names(case = "snake") ## changes all column names to snake_case (lowercase and spaces replaces with)
+    janitor::clean_names(case = "snake") ## changes all column names to snake_case (lowercase and spaces replaced with)
 
   return(tidied_data)
 }
@@ -30,9 +32,7 @@ cleaned_Data <-tidy_data(data)
 
 
 
-
-
-## Function 2
+## Function 2: Making this dataset available as part of this R package
 
 load_data <- function(data){
 
@@ -44,20 +44,23 @@ load_data(cleaned_Data)
 
 
 
-##Function 3
+##Function 3: Plotting inputted x vs. y
 
 plot.tidy_data <- function(object, x, y, ...) {
+
   if (missing(x) || missing(y)) {
+
     stop("You must specify both x and y for the plot.")
+
   }
+
   ggplot(object, aes(x = .data[[x]], y = .data[[y]] )) +
     geom_point() +
     theme_minimal() +
     labs(title = "Tidy Data Plot")
 }
 
+
 plot.tidy_data(cleaned_Data, "SPAD", "chlorophyll")
 
 devtools::check()
-usethis::use_gpl_license()
-
